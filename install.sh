de4a83784139959a2bbcaefe5e7600378bcce743
Rodrigo Dias Botelho Pellicano <rodrigo.pellicano@planejamento.gov.br>
2018-11-09 16:26:45 -0200

    Corrigindo o install quando esta na pasta do go

--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ go get -u github.com/golang/protobuf/protoc-gen-go
 echo "É preciso ter o protoc instalado no seu linux"
 
 cp adiciona_transport.sh ${HOME}/go/bin/.
-mkdir -p ${HOME}/go/src/github.com/rodrigobotelho/buildtransports
-cp -r templates/* ${HOME}/go/src/github.com/rodrigobotelho/buildtransports/.
+if [ ${PWD} != "${HOME}/go/src/github.com/rodrigobotelho/buildtransports" ] ;then
+    mkdir -p ${HOME}/go/src/github.com/rodrigobotelho/buildtransports
+    cp -r * ${HOME}/go/src/github.com/rodrigobotelho/buildtransports/.
+fi
 
 RUN=${HOME}/go/bin/adiciona_transport.sh
 cat ${RUN} |sed 's/PACKAGE=.*/PACKAGE=${HOME}\/go\/src\/github.com\/rodrigobotelho\/buildtransports\/templates/g' > ${RUN}_tmp
