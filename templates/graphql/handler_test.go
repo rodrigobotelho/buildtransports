b8da1bb28517d41e60bdedcffade3a26b199a36e
Marcos César de Oliveira <mcesarhm@gmail.com>
2018-12-03 15:35:38 -0200

    Troca hardcoded secret por uma váriavel do pacote graphqlkit

--- templates/graphql/handler_test.go
+++ templates/graphql/handler_test.go
@@ -22,7 +22,7 @@ func TestHandler(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			svc := service.NewBasicExampleService()
-			h := NewHandler(svc, "schema.graphql", "#yuui123", nil)
+			h := NewHandler(svc, "schema.graphql", string(graphqlkit.Secret), nil)
 			mux := http.NewServeMux()
 			mux.Handle("/", h.Handler())
 			for _, args := range tt.args {
